21bf4bf7040a2d86a33cdc00ad3c2d0203d9cb99
JenJen123123 <ssystem32@gmail.com>
2025-07-14 23:22:57 +0300

    everything works

--- modules/ec2/init.sh
+++ modules/ec2/init.sh
@@ -26,34 +26,46 @@ echo "[INFO] Running playbook..."
 #ansible-playbook -i inventory.ini pokemon_stack.yml
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini pokemon_stack.yml
 
-
-
-
-
-
-
-
-
-
-
-
 # *****************************************************************************
 
-###############################################################################
+# Install AWS CLI
+echo "[INFO] Installing AWS CLI..."
+# Install dependencies
+sudo apt update && sudo apt install -y unzip curl
+
+# Download the installer
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Unzip
+unzip awscliv2.zip
+
+# Run the installer
+sudo ./aws/install
+
+# Verify
+aws --version
+
+
 
 # Game setup
-# cd /home/ubuntu
-# git clone https://github.com/WhiskersStack/PokemonWithDynamoDB.git
-# chown -R ubuntu:ubuntu /home/ubuntu/PokemonWithDynamoDB
-# echo 'if [ -n "$SSH_CONNECTION" ]; then cd ~/pokemon-ansible; fi' >> /home/ubuntu/.bashrc
+cd /home/ubuntu
+git clone https://github.com/WhiskersStack/PokemonWithMongo.git
+chown -R ubuntu:ubuntu /home/ubuntu/PokemonWithMongo
+cat <<'EOF' >> /home/ubuntu/.bashrc
 
-# Ansible setup
-# sudo apt update -y
-# sudo apt install software-properties-common -y
-# sudo add-apt-repository --yes --update ppa:ansible/ansible
-# sudo apt install ansible -y
-# ansible --version
-# ansible-galaxy collection install community.docker
+# Auto-launch Pokemon game on SSH
+if [ -n "$SSH_CONNECTION" ]; then
+  export PRIVATE_IP_DB=$(aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=Database" \
+    --query "Reservations[*].Instances[*].PrivateIpAddress" \
+    --output text)
+  cd ~/PokemonWithMongo && python3 main.py
+fi
+EOF
 
-# cd ~/pokemon-ansible
-# ansible-playbook -i inventory.ini pokemon_stack.yml --extra-vars="@vars.yml"
+
+
+
+
+
+echo "All done! You can now run the game by SSHing into the instance and executing the game script."
